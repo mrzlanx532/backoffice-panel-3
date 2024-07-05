2d752d3581e50dc2fba538cdd8acfe70d2dd9980
Denis Danilov <d.danilov@duotek.ru>
2024-07-06 00:06:52 +0300

    Temp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,10 +7,6 @@ export default defineNuxtConfig({
 
   devtools: { enabled: true },
 
-  plugins: [
-    'plugins/scrollable.ts',
-  ],
-
   laravelAuth: {
     domain: 'http://backoffice-api.lsmlocal.ru',
     redirects: {
--- plugins-from-lib/scroll/helpers.js
+++
@@ -1,7 +0,0 @@
-export default function createElementWithClass(className) {
-    const el = document.createElement('div')
-
-    el.setAttribute('class', className)
-
-    return el
-}
