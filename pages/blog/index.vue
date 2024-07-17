b01f31f1d4959ccdf9181399b3d521b6de3fd1d6
Denis Danilov <d.danilov@duotek.ru>
2024-07-17 17:34:43 +0300

    Temp

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,111 @@
+<script setup lang="ts">
+import Button from '@/components/Base/Button.vue'
+import Section from '@/components/Base/Section.vue'
+import Browser from '@/components/Base/Browser/Browser.vue';
+
+import { definePageMeta, useNuxtApp } from '#imports'
+import type { Ref } from 'vue'
+
+definePageMeta({
+  middleware: ['auth']
+})
+
+const { $modal, $notification, $authFetch } = useNuxtApp()
+
+const item: Ref<{}|null> = ref({})
+
+const browserEl: Ref<HTMLElement|null> = ref(null)
+
+const columns = shallowRef([
+  {
+    name: 'id',
+    title: 'ID'
+  },
+  {
+    name: 'date',
+    title: 'Дата',
+  },
+  {
+    name: 'name',
+    title: 'Заголовок',
+  },
+  {
+    name: 'category',
+    title: 'Рубрика',
+    toFormat(item) {
+      return item?.category?.name_ru
+    }
+  },
+  {
+    name: 'locale',
+    title: 'Локаль',
+    toFormat(item) {
+      return item?.locale?.title
+    }
+  },
+  {
+    name: 'state',
+    title: 'Статус',
+    toFormat(item) {
+      return item?.state?.title
+    }
+  },
+  {
+    name: 'created_at',
+    title: 'Создан',
+    preset: {
+      name: 'timestampToFormatPreset'
+    }
+  },
+])
+
+const requestProperties = ref([
+  'id',
+  'date',
+  'name',
+  'category',
+  'locale',
+  'state',
+  'created_at',
+  'company_business_type',
+  'job_title',
+  'company_address',
+  'company_index',
+  'company_country',
+  'company_city',
+  'subscription_type'
+])
+
+const onItemUpdated = (newItem) => {
+    item.value = newItem
+}
+
+const onClickDelete = () => {
+  $modal.confirm().then(async (isAgree) => {
+    if (!isAgree) {
+      return
+    }
+
+    await $authFetch(`http://backoffice-api.lsmlocal.ru/blog/posts/delete`, {
+      method: 'POST',
+      body: {
+        id: item.value.id,
+      },
+    })
+
+    $notification.push({type: 'success', message: 'Статья удалена'})
+    browserEl.value.reset()
+  })
+}
+
+const onClickEdit = () => {
+
+}
+</script>
+
 <template>
   <Browser
+      ref="browserEl"
       h1="Статьи блога"
       url-prefix="blog/posts"
       :columns="columns"
@@ -12,7 +118,7 @@
     <template #browserDetailHeader>
       <div class="btn__group">
         <Button @click="onClickEdit" :classes="['--big --outline-primary']">Изменить</Button>
-        <Button @click="onClickEdit" :classes="['--big --outline-danger']">Удалить</Button>
+        <Button @click="onClickDelete" :classes="['--big --outline-danger']">Удалить</Button>
       </div>
     </template>
     <template #browserDetailContent>
@@ -22,7 +128,7 @@
             Основное
           </template>
           <template v-slot:content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus blanditiis consequatur enim fugit id molestias necessitatibus obcaecati quidem repudiandae. Cumque cupiditate doloribus ex excepturi vel. Dolores eius nam quo rerum.
+            {{ item.name }}
           </template>
         </Section>
         <Section>
@@ -30,135 +136,10 @@
             Дополнительное
           </template>
           <template v-slot:content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus blanditiis consequatur enim fugit id molestias necessitatibus obcaecati quidem repudiandae. Cumque cupiditate doloribus ex excepturi vel. Dolores eius nam quo rerum.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque culpa ea eius ipsa labore maxime molestiae perferendis perspiciatis quaerat quod saepe sequi, sint suscipit temporibus ut velit, voluptatibus! Consectetur?
+            <div v-html="item.content"/>
           </template>
         </Section>
       </div>
     </template>
   </Browser>
 </template>
-<script>
-import PageWithBrowserMixin from "@/mixins/pages/browser.js"
-import SubscriptionRow from "@/components/CustomRows/users/SubscriptionRow";
-import { getMethods } from "@/mixins/browser";
-import Button from "@/components/Base/Button"
-import Section from "@/components/Base/Section";
-import {definePageMeta} from "#imports";
-
-export default {
-  setup() {
-    definePageMeta({
-      middleware: ['auth'],
-    })
-  },
-  name: 'BlogPage',
-  mixins: [PageWithBrowserMixin],
-  components: {
-    Button,
-    SubscriptionRow,
-    Section
-  },
-  data() {
-    return {
-      requestProperties: [
-          'id',
-          'date',
-          'name',
-          'category',
-          'locale',
-          'state',
-          'created_at',
-          'company_business_type',
-          'job_title',
-          'company_address',
-          'company_index',
-          'company_country',
-          'company_city',
-          'subscription_type'
-      ],
-      columns: [
-        {
-          name: 'id',
-          title: 'ID'
-        },
-        {
-          name: 'date',
-          title: 'Дата',
-        },
-        {
-          name: 'name',
-          title: 'Заголовок',
-        },
-        {
-          name: 'category',
-          title: 'Рубрика',
-          toFormat(item) {
-            return item?.category?.name_ru
-          }
-        },
-        {
-          name: 'locale',
-          title: 'Локаль',
-          toFormat(item) {
-            return item?.locale?.title
-          }
-        },
-        {
-          name: 'state',
-          title: 'Статус',
-          toFormat(item) {
-            return item?.state?.title
-          }
-        },
-        {
-          name: 'created_at',
-          title: 'Создан',
-          preset: {
-            name: 'timestampToFormatPreset'
-          }
-        },
-      ],
-    }
-  },
-  methods: {
-    ...getMethods,
-    onClickEdit() {
-      console.log(123)
-    },
-  }
-}
-</script>
