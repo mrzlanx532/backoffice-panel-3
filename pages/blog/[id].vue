530468307cce3fd6e258b924d18a016ed363962e
Denis Danilov <d.danilov@duotek.ru>
2024-09-08 14:41:02 +0300

    Temp

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -94,15 +94,3 @@ const h1 = ref('Пост ' + entityId)
     </template>
   </Detail>
 </template>
-
-<style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity .3s;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
