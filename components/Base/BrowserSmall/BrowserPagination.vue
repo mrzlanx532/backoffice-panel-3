9a06ad1eb582383567c0609a14c9d7e88412ce64
Denis Danilov <d.danilov@duotek.ru>
2024-09-15 23:35:20 +0300

    Temp

--- components/Base/BrowserSmall/BrowserPagination.vue
+++ components/Base/BrowserSmall/BrowserPagination.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+const emit = defineEmits(['changePage'])
+
 import BrowserPagination from '~/components/Base/Browser/BrowserPagination.vue'
 
 const props = withDefaults(defineProps<{
-  total?: number
+  total?: number,
+  page?: number,
 }>(), {
-  total: 0
+  total: 0,
+  page: 1
 })
+
+const onChangePage = (page: number): void => {
+  emit('changePage', page)
+}
 </script>
 
 <template>
@@ -14,9 +22,11 @@ const props = withDefaults(defineProps<{
       Всего: {{props.total}}
     </div>
     <BrowserPagination
-        :per-page="20"
+        @change-page="onChangePage"
+        :page="props.page"
+        :per-page="10"
         :total="props.total"
-        style="margin-top: 6px;"
+        style="margin-top: 10px;"
     />
   </div>
 </template>
