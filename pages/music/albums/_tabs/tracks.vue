264ce7c13f3179ea4c0ab90f4bfce10be58e352a
Denis Danilov <d.danilov@duotek.ru>
2024-10-01 17:57:15 +0300

    Temp

--- pages/music/albums/_tabs/tracks.vue
+++ pages/music/albums/_tabs/tracks.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import BrowserSmall, { type IColumn } from '~/components/Base/BrowserSmall/BrowserSmall.vue'
-import type { Ref } from 'vue'
-import { prepareDuration } from '~/helpers/functions-for-table-columns'
+import BrowserSmall from '~/components/Base/BrowserSmall/BrowserSmall.vue'
 import TracksTr from '~/components/CustomTr/TrackTr.vue'
 
 type IItem = {[key: string]: any}
@@ -9,31 +7,6 @@ type IItem = {[key: string]: any}
 const props = defineProps<{
   item?: IItem,
 }>()
-
-const columns: Ref<IColumn[]> = ref([
-  {
-    name: 'name',
-    title: 'Название',
-    toFormat: (item: IItem) => {
-      if (item.title_ru) {
-        return `${item.title_en} (${item.title_ru})`
-      }
-
-      return item.title_en
-    }
-  },
-  {
-    name: 'duration',
-    title: 'Продолжительность',
-    toFormat: (item: IItem) => {
-      if (item.variation && item.variation.duration) {
-        return prepareDuration(item.variation.duration)
-      }
-
-      return '-'
-    }
-  }
-])
 
 const filters = ref({
   'album_id': [props.item!.id]
