0ee5178843e638601d9dfb36a9fabd18ab5a8bf2
Denis Danilov <d.danilov@duotek.ru>
2024-10-13 13:48:30 +0300

    Temp

--- pages/music/authors/index.vue
+++ pages/music/authors/index.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-import type { Ref } from 'vue'
 import { definePageMeta } from '#imports'
 
-import Browser, { type IItem } from '~/components/Base/Browser/Browser.vue'
+import Browser from '~/components/Base/Browser/Browser.vue'
+import MainTab from '~/pages/music/authors/_tabs/main.vue'
+import TracksTab from '~/pages/music/authors/_tabs/tracks.vue'
+import AlbumsTab from '~/pages/music/authors/_tabs/albums.vue'
+import Button from '~/components/Base/Button.vue'
+import Tabs from '~/components/Base/Tabs.vue'
 
 definePageMeta({
   middleware: ['auth']
 })
-
-const item: Ref<IItem|null> = ref(null)
 
 const requestProperties = ref([
   'id',
@@ -59,13 +61,41 @@ const columns = ref([
   },
 ])
 
-const onItemUpdated = (item: IItem) => {
-  item.value = item
-}
+const {
+  browserEl,
+  item,
+
+  onClickCreate,
+  onClickEdit,
+  onClickDelete,
+  onItemUpdated,
+} = usePage()
+
+const { initTabs } = useTabs()
+
+const {
+  tabs,
+  selectedTabComponent,
+  onChangeSelectedTab
+} = initTabs([
+  {
+    title: 'Инфо',
+    component: MainTab
+  },
+  {
+    title: 'Треки',
+    component: TracksTab
+  },
+  {
+    title: 'Альбомы',
+    component: AlbumsTab
+  }
+])
 </script>
 
 <template>
   <Browser
+      ref="browserEl"
       h1="Авторы музыкальных треков"
       url-prefix="music/authors/browse"
 
@@ -78,5 +108,35 @@ const onItemUpdated = (item: IItem) => {
       :request-properties="requestProperties"
 
       @itemUpdated="onItemUpdated"
-  />
+  >
+    <template #rightSide>
+      <div class="btn__group">
+        <Button @click="onClickCreate({
+          formURL: 'music/authors/form',
+          modalPath: 'music/authors/form',
+          modalTitle: 'Создать автора',
+          notificationMessage: 'Автор создан'
+        })" :class="['--small --success']">Добавить</Button>
+      </div>
+    </template>
+    <template #browserDetailHeader>
+      <div class="btn__group">
+        <Button @click="onClickEdit({
+          formURL: 'music/authors/form',
+          modalPath: 'music/authors/form',
+          modalTitle: 'Изменить автора',
+          notificationMessage: 'Автор изменен'
+        })" :class="['--big --outline-primary']">Изменить</Button>
+        <Button @click="onClickDelete({
+          deleteURL: 'music/authors/delete',
+          notificationMessage: 'Автор удален'
+        })" :class="['--big --outline-danger']">Удалить</Button>
+      </div>
+      <Tabs @change="onChangeSelectedTab" :tabs="tabs"/>
+    </template>
+
+    <template #browserDetailContent>
+      <component :is="selectedTabComponent" :item="item"/>
+    </template>
+  </Browser>
 </template>
