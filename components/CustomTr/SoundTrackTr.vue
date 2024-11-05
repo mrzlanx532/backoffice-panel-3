c5d81b1da163d4f6e7779662746dd2f7404eab29
Denis Danilov <d.danilov@duotek.ru>
2024-11-06 00:07:19 +0300

    Temp

---
+++ components/CustomTr/SoundTrackTr.vue
@@ -0,0 +1,117 @@
+<script setup lang="ts">
+import Button from '~/components/Base/Button.vue'
+import { prepareDuration } from '~/helpers/functions-for-table-columns'
+import ButtonDropdown from '~/components/Base/ButtonDropdown.vue'
+import { useNuxtApp } from '#imports'
+import type { ComponentInternalInstance } from 'vue'
+
+type IItem = Record<any, string>
+
+const {
+  $authFetch,
+  $modal,
+  $notification
+} = useNuxtApp()
+
+const props = defineProps<{
+  item: IItem,
+  browserSmallEl: ComponentInternalInstance | null
+}>()
+
+const onClickEdit = async () => {
+  const formResponse = await $authFetch('sound/tracks/form', {
+    method: 'GET',
+    params: {
+      id: props.item.id,
+    },
+  })
+
+  $modal.load('sound/form', {
+    title: 'Изменить трек',
+    id: props.item.id,
+    formResponse
+  }).then(() => {
+    $notification.push({type: 'success', message: 'Трек изменен'})
+  })
+}
+
+const onClickDelete = async () => {
+  $modal.confirm().then(async (isAgree: boolean) => {
+    if (!isAgree) {
+      return
+    }
+
+    await $authFetch('sound/tracks/delete', {
+      method: 'POST',
+      body: {
+        id: props.item.id,
+      },
+    })
+
+    if (props.browserSmallEl?.exposed) {
+      props.browserSmallEl.exposed.reset()
+    }
+
+    $notification.push({type: 'success', message: 'Трек удален'})
+  })
+}
+
+const onClickDownload = () => {
+  alert('Скачиваем')
+}
+
+const buttonDropdownItems = [
+  { title: 'Скачать', class: '--primary', onClick: onClickDownload },
+  { title: 'Изменить', class: '--primary', onClick: onClickEdit },
+  { title: 'Удалить', class: '--danger', onClick: onClickDelete },
+]
+
+const onClickListen = () => {
+  window.alert('Слушаем песню..')
+}
+
+const onClickInfo = () => {
+  window.open('/sounds/' + props.item.id)
+}
+</script>
+
+<template>
+  <tr class="--disable-cursor --disable-hover">
+    <td>
+      <p>{{ props.item.name }}</p>
+      <p style="color: grey;">{{ props.item.is_main ? 'Основная' : '' }}</p>
+    </td>
+    <td class="--min-width">
+      {{ props.item.file_size + ' MB' }}
+    </td>
+    <td class="--min-width">
+      {{ props.item.duration ? prepareDuration(props.item.duration) : '-' }}
+    </td>
+    <td class="--min-width">
+      <Button class="--small --success" @click="onClickListen">
+        Слушать
+      </Button>
+    </td>
+    <td class="--min-width">
+      <svg @click="onClickInfo()">
+        <use xlink:href="/img/sprite.svg#primary"/>
+      </svg>
+    </td>
+    <td class="--min-width">
+      <ButtonDropdown :items="buttonDropdownItems"/>
+    </td>
+  </tr>
+</template>
+
+<style scoped>
+svg {
+  width: 20px;
+  height: 20px;
+  color: grey;
+  cursor: pointer;
+}
+
+svg:hover {
+  color: orange;
+}
+</style>
--- pages/sounds/_tabs/variations.vue
+++ pages/sounds/_tabs/variations.vue
@@ -1,7 +1,61 @@
 <script setup lang="ts">
+import BrowserSmall from '~/components/Base/BrowserSmall/BrowserSmall.vue'
+import SoundTrackTr from '~/components/CustomTr/SoundTrackTr.vue'
 
+const props = defineProps<{
+  item: IItem,
+}>()
+
+type IItem = Record<string, any>
+
+const filters = ref({
+  'track_id': [props.item!.id]
+})
+
+const columns = ref([
+  {
+    name: '_',
+    title: 'Название и свойства'
+  },
+  {
+    name: '_',
+    title: 'Размер',
+  },
+  {
+    name: '_',
+    title: 'Время',
+  },
+  {
+    name: '_',
+    title: 'Обработка',
+  },
+  {
+    name: '_',
+    title: '',
+  },
+  {
+    name: '_',
+    title: '',
+  },
+]);
+
+watch(
+    () => props.item,
+    (value) => {
+      filters.value = {
+        'track_id': [value!.id]
+      }
+    }
+)
 </script>
 
 <template>
-
+  <BrowserSmall
+      h1="Вариации"
+      url-prefix="sound/tracks/variations/browse"
+      :filters="filters"
+      :columns="columns"
+      :is-enabled-search="true"
+      :custom-tr="SoundTrackTr"
+  />
 </template>
